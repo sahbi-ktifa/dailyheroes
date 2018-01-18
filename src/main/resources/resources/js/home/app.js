50f5f7800cbc01193e809f6fb9b2c5a0a9320bd9
sktifa <sahbi.ktifa@4dconcept.fr>
2018-01-18 15:58:54 +0100

    1.0.0.BETA2 version

--- src/main/resources/resources/js/home/app.js
+++ src/main/resources/resources/js/home/app.js
@@ -28,6 +28,7 @@
  * Mobile menu hack
  * */
 $('.nav a').on('click', function(){
-    $('.btn-navbar').click(); //bootstrap 2.x
-    $('.navbar-toggle').click() //bootstrap 3.x by Richard
+    if ($('.navbar-collapse').width() < 990) {
+        $('.navbar-toggle').click();
+    }
 });
